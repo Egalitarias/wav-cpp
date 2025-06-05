26dd760d21d1aba204ceb1cc22091b1ccdb2bffd
Egalitarias <garydavieshome@gmail.com>
2025-06-05 08:22:25 +0800

    SineOscillator

--- wav-cpp/wav-cpp/wav-cpp.cpp
+++ wav-cpp/wav-cpp/wav-cpp.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <cmath>
+
+const int SAMPLE_RATE = 44100;
+const float PI = 3.14159;
+
+class SineOscillator {
+    float frequency;
+    float amplitude;
+    float angle = 0.0f;
+
+public:
+    SineOscillator(float theFrequency, float theAmplitude): frequency(theFrequency), amplitude(theAmplitude) {}
+    float process() {
+        float sample = amplitude * sin(angle);
+        angle += 2 * PI * frequency / SAMPLE_RATE;
+        return sample;
+    }
+};
 
 int main()
 {
