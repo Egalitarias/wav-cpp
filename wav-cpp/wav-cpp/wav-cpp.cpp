bfdb2a3ccbcbb667a98d399ba56ecd182acad4c6
Egalitarias <garydavieshome@gmail.com>
2025-06-05 08:37:41 +0800

    Write samples to file

--- wav-cpp/wav-cpp/wav-cpp.cpp
+++ wav-cpp/wav-cpp/wav-cpp.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 const int SAMPLE_RATE = 44100;
 const float PI = 3.14159;
@@ -8,17 +9,28 @@ class SineOscillator {
     float frequency;
     float amplitude;
     float angle = 0.0f;
+    float offset;
 
 public:
-    SineOscillator(float theFrequency, float theAmplitude): frequency(theFrequency), amplitude(theAmplitude) {}
+    SineOscillator(float theFrequency, float theAmplitude): frequency(theFrequency), amplitude(theAmplitude) {
+        offset = PI * frequency / SAMPLE_RATE;
+    }
     float process() {
         float sample = amplitude * sin(angle);
-        angle += 2 * PI * frequency / SAMPLE_RATE;
+        angle += offset;
         return sample;
     }
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    int duration = 2;
+    std::ofstream audioFile;
+    audioFile.open("waveform");
+    SineOscillator SineOscillator(440, 0.5);
+
+    for (int i = 0; i < SAMPLE_RATE * duration; ++i) {
+        audioFile << SineOscillator.process() << std::endl;
+    }
+
+    audioFile.close();
 }
